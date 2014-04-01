fdc6a4def0d262977f9f88b3ef2f0edd2e4fee1e
sirMackk <c.tairix@gmail.com>
2014-04-01 14:40:14 +0100

    Fixes beeline color

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   hljs.initHighlighting();
   var postContent = document.getElementsByClassName('post');
   if (postContent.length > 0) {
-    var beeLine = new BeeLineReader(postContent[0], { theme: 'bright', skipBackgroundColor: true, skipTags: ['pre', 'code', 'h1', 'h2', 'h3', 'h4', 'h5']});
+    var beeLine = new BeeLineReader(postContent[0], { theme: 'dark', skipBackgroundColor: true, skipTags: ['pre', 'code', 'h1', 'h2', 'h3', 'h4', 'h5']});
     beeLine.color();
   };
 });
